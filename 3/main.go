d8d1ed42c3397db194805e89a2bc1c795120c78e
Will Simpson <wsimpson@bignerdranch.com>
2021-12-07 17:20:26 -0700

    Add solution for part 2 of day 3

--- 3/main.go
+++ 3/main.go
@@ -3,75 +3,73 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
-	"sort"
+	"strconv"
+	"strings"
 )
 
 func main() {
-
-	f, err := os.Open("input.txt")
-
+	content, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	var setBits []uint
-	var allLines []string
-	var numLines, gammaRate, epsilonRate uint
-	for scanner.Scan() {
-		line := scanner.Text()
-		allLines = append(allLines, line)
-		if err != nil {
-			log.Fatal(err)
+	lines := strings.Split(string(content), "\n")
+	bitSize := len(lines[0])
+	var nums []int64
+	for _, v := range lines {
+		b, err := strconv.ParseInt(v, 2, 64)
+		if err == nil {
+			nums = append(nums, b)
 		}
+	}
+	co2nums := nums
 
-		if setBits == nil {
-			setBits = make([]uint, len(line))
+	for i := 1; i <= bitSize; i++ {
+		var set, unset []int64
+		if len(nums) == 1 {
+			break
 		}
-
-		for i, c := range line {
-			if c == '1' {
-				setBits[i]++
+		for _, v := range nums {
+			if v&(1<<(bitSize-i)) > 0 {
+				set = append(set, v)
+			} else {
+				unset = append(unset, v)
 			}
 		}
-		numLines++
-	}
-	offset := len(setBits) - 1
-	for k, v := range setBits {
-		if v >= numLines/2 {
-			gammaRate |= (1 << (offset - k))
+		if len(set) > len(unset) {
+			nums = set
+		} else if len(set) < len(unset) {
+			nums = unset
 		} else {
-			epsilonRate |= (1 << (offset - k))
+			nums = set
 		}
 	}
+	fmt.Printf("o2: %05b\n", nums[0])
 
-	o2prefix := "10111"
-
-	sort.Sort(sort.StringSlice(allLines))
-	for _, v := range allLines {
-		fmt.Println(v)
-	}
-	fmt.Printf("o2prefix: %s\n", o2prefix)
-	o2 := splitSliceByPrefixHelper(0, o2prefix, allLines)
-	fmt.Printf("The Oxygen Level is: %s\n", o2)
-}
-
-func splitSliceByPrefixHelper(index int, prefix string, slice []string) string {
-	fmt.Printf("full prefix: %s\t prefix segment: %c index: %d\n", prefix, prefix[index], index)
-	if len(prefix)-1 == index || len(slice) == 1 {
-		return slice[0]
-	}
-	for k, v := range slice {
-		if prefix[index] == v[index] {
-			fmt.Printf("\tNew slice %s\n", slice[k:])
-			return splitSliceByPrefixHelper(index+1, prefix, slice[k:])
+	for i := 1; i <= bitSize; i++ {
+		var set, unset []int64
+		if len(co2nums) == 1 {
+			break
+		}
+		for _, v := range co2nums {
+			if v&(1<<(bitSize-i)) > 0 {
+				set = append(set, v)
+			} else {
+				unset = append(unset, v)
+			}
+		}
+		if len(set) < len(unset) {
+			co2nums = set
+		} else if len(set) > len(unset) {
+			co2nums = unset
+		} else {
+			co2nums = unset
 		}
 	}
-	return slice[0]
+	fmt.Printf("co2: %05b\n", co2nums[0])
+	fmt.Printf("Life support rating: %d\n", nums[0]*co2nums[0])
 }
 
 func main_part1() {
